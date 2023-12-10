2cca2dd42afec8959442f6a4c6772cfbbe380bc9
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:15:10 +0530

    notes.py

--- TitanXManager/modules/notes.py
+++ TitanXManager/modules/notes.py
@@ -19,13 +19,13 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_markdown
 
-import FallenRobot.modules.sql.notes_sql as sql
-from FallenRobot import DRAGONS, EVENT_LOGS, LOGGER, SUPPORT_CHAT, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import connection_status, user_admin
-from FallenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from FallenRobot.modules.helper_funcs.msg_types import get_note_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+import TitanXManager.modules.sql.notes_sql as sql
+from TitanXManager import DRAGONS, EVENT_LOGS, LOGGER, SUPPORT_CHAT, dispatcher
+from TitanXManager.modules.disable import DisableAbleCommandHandler
+from TitanXManager.modules.helper_funcs.chat_status import connection_status, user_admin
+from TitanXManager.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from TitanXManager.modules.helper_funcs.msg_types import get_note_type
+from TitanXManager.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 
