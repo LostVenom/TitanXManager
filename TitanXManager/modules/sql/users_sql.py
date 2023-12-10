1b8ed38ad26d315172f7e39a655ea13006186efb
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 20:41:49 +0530

    users_sql.py

--- TitanXManager/modules/sql/users_sql.py
+++ TitanXManager/modules/sql/users_sql.py
@@ -10,8 +10,8 @@ from sqlalchemy import (
     func,
 )
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.sql import BASE, SESSION
+from TitanXManager import dispatcher
+from TitanXManager.modules.sql import BASE, SESSION
 
 
 class Users(BASE):
