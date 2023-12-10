9f3de0304e22565bb27b20f3e11618cf537a8ed1
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:05:22 +0530

    remote_cmds.py

--- TitanXManager/modules/remote_cmds.py
+++ TitanXManager/modules/remote_cmds.py
@@ -2,15 +2,15 @@ from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from TitanXManager import LOGGER, dispatcher
+from TitanXManager.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user_and_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
+from TitanXManager.modules.helper_funcs.extraction import extract_user_and_text
+from TitanXManager.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
