e5d1902c0e9db82f4f76729526a7c165e9ec884b
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 20:30:16 +0530

    dbcleanup.py

--- TitanXManager/modules/dbcleanup.py
+++ TitanXManager/modules/dbcleanup.py
@@ -4,10 +4,10 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 
-import FallenRobot.modules.sql.global_bans_sql as gban_sql
-import FallenRobot.modules.sql.users_sql as user_sql
-from FallenRobot import DEV_USERS, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import TitanXManager.modules.sql.global_bans_sql as gban_sql
+import TitanXManager.modules.sql.users_sql as user_sql
+from TitanXManager import DEV_USERS, OWNER_ID, dispatcher
+from TitanXManager.modules.helper_funcs.chat_status import dev_plus
 
 
 def get_invalid_chats(update: Update, context: CallbackContext, remove: bool = False):
