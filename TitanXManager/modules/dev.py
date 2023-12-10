e1757a3e9ebf11845e30b2cf5c0c03b83c0c49a2
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 20:31:47 +0530

    dev.py

--- TitanXManager/modules/dev.py
+++ TitanXManager/modules/dev.py
@@ -8,9 +8,9 @@ from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler
 
-import FallenRobot
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import TitanXManager
+from TitanXManager import dispatcher
+from TitanXManager.modules.helper_funcs.chat_status import dev_plus
 
 
 @dev_plus
