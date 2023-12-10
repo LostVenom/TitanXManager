d3ab6ca3b165f48f6794b7425835ddb8131f32b1
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:36:11 +0530

    group.py

--- TitanXManager/modules/group.py
+++ TitanXManager/modules/group.py
@@ -4,6 +4,6 @@ __help__ = """
  ❍ /delgpic*:* Same as above but to remove group profile pic.
  ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
  ❍ /setdescription <description>*:* Sets new chat description in group.
-"""
+""" 
 
 __mod_name__ = "Gʀᴏᴜᴘ"
