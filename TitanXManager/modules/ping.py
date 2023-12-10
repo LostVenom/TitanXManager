a167c27f8520832e5c5547f1f63f5d55f8e6b1b7
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:02:33 +0530

    ping.py

--- TitanXManager/modules/ping.py
+++ TitanXManager/modules/ping.py
@@ -3,8 +3,8 @@ import time
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
-from FallenRobot import StartTime, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from TitanXManager import StartTime, dispatcher
+from TitanXManager.modules.disable import DisableAbleCommandHandler
 
 
 def get_readable_time(seconds: int) -> str:
@@ -39,13 +39,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
+    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
+        "ɪ ᴀᴍ ᴀʟɪᴠᴇ! 🖤\n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
