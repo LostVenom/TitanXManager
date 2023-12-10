873540ca3c46f305373ef30629d953761e843fc7
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 20:23:58 +0530

    chatbot_sql.py

--- TitanXManager/modules/sql/chatbot_sql.py
+++ TitanXManager/modules/sql/chatbot_sql.py
@@ -2,41 +2,41 @@ import threading
 
 from sqlalchemy import Column, String
 
-from FallenRobot.modules.sql import BASE, SESSION
+from TitanXManager.modules.sql import BASE, SESSION
 
 
-class FallenChats(BASE):
-    __tablename__ = "fallen_chats"
+class TitanXSupport(BASE):
+    __tablename__ = "TitanXSupport"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
 
-FallenChats.__table__.create(checkfirst=True)
+TitanXSupport.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_fallen(chat_id):
+def is_titan(chat_id):
     try:
-        chat = SESSION.query(FallenChats).get(str(chat_id))
+        chat = SESSION.query(TitanXSuport).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
 
-def set_fallen(chat_id):
+def set_titan(chat_id):
     with INSERTION_LOCK:
-        fallenchat = SESSION.query(FallenChats).get(str(chat_id))
-        if not fallenchat:
-            fallenchat = FallenChats(str(chat_id))
-        SESSION.add(fallenchat)
+        titanxsupport = SESSION.query(TitanXSupport).get(str(chat_id))
+        if not TitanXSupport:
+            titanxsupport = TitanXSupport(str(chat_id))
+        SESSION.add(titanxsupport)
         SESSION.commit()
 
 
-def rem_fallen(chat_id):
+def rem_titan(chat_id):
     with INSERTION_LOCK:
-        fallenchat = SESSION.query(FallenChats).get(str(chat_id))
-        if fallenchat:
-            SESSION.delete(fallenchat)
+        titanxsupport = SESSION.query(TitanXSupport).get(str(chat_id))
+        if titanxsupport:
+            SESSION.delete(titanxsupport)
         SESSION.commit()
