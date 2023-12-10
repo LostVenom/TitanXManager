251be955910325b0a47bbbccf031a2c566aa155e
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 22:25:36 +0530

    wallpaper.py

--- TitanXManager/modules/wallpaper.py
+++ TitanXManager/modules/wallpaper.py
@@ -4,7 +4,7 @@ import requests
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from FallenRobot import pbot
+from TitanXManager import pbot
 
 ##TO-DO
 
