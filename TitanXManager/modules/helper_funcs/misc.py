0f37da819c8d0383f94b2707be2f8550ba5b96e6
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-10 19:44:24 +0530

    Update misc.py

--- TitanXManager/modules/helper_funcs/misc.py
+++ TitanXManager/modules/helper_funcs/misc.py
@@ -13,7 +13,7 @@ from telegram import (
 )
 from telegram.error import TelegramError
 
-from FallenRobot import NO_LOAD
+from TitanXManager import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
