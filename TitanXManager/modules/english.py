fc75bc0889a0a6d6dd1537d32b9e7e96bbbcc6e9
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-10 20:50:44 +0530

    Update english.py

--- TitanXManager/modules/english.py
+++ TitanXManager/modules/english.py
@@ -3,7 +3,7 @@ import json
 import requests
 from PyDictionary import PyDictionary
 
-from FallenRobot.events import register
+from TitanXManager.events import register
 
 API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
 URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
