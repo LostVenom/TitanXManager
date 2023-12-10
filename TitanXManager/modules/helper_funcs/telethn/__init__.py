aa4f58818b3af4d536814aef643553ef383b852d
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-10 18:38:57 +0530

    Update __init__.py

--- TitanXManager/modules/helper_funcs/telethn/__init__.py
+++ TitanXManager/modules/helper_funcs/telethn/__init__.py
@@ -1,4 +1,4 @@
-from FallenRobot import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, telethn
+from TitanXManager import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, telethn
 
 IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
 
