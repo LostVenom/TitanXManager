949637a0ccdf837ad5b0fc7e524675db2fba8f8f
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 19:54:33 +0530

    afk.py

--- TitanXManager/modules/afk.py
+++ TitanXManager/modules/afk.py
@@ -5,13 +5,13 @@ from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import (
+from TitanXManager import dispatcher
+from TitanXManager.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from FallenRobot.modules.sql import afk_sql as sql
-from FallenRobot.modules.users import get_user_id
+from TitanXManager.modules.sql import afk_sql as sql
+from TitanXManager.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
