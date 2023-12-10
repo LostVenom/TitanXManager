52a9dafb9cea622531d94ab5aeeefbc91d39b1e1
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:13:29 +0530

    sed.py

--- TitanXManager/modules/sed.py
+++ TitanXManager/modules/sed.py
@@ -5,9 +5,9 @@ import telegram
 from telegram import Update
 from telegram.ext import CallbackContext, Filters
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.disable import DisableAbleMessageHandler
-from FallenRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from TitanXManager import LOGGER, dispatcher
+from TitanXManager.modules.disable import DisableAbleMessageHandler
+from TitanXManager.modules.helper_funcs.regex_helper import infinite_loop_check
 
 DELIMITERS = ("/", ":", "|", "_")
 
