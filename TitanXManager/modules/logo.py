726bf6d913974e624c2668bef5cb10efcad00dfd
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 23:29:55 +0530

    logo.py

--- TitanXManager/modules/logo.py
+++ TitanXManager/modules/logo.py
@@ -6,8 +6,8 @@ import random
 import requests
 from PIL import Image, ImageDraw, ImageFont
 
-from FallenRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, telethn
-from FallenRobot.events import register
+from TitanXManager import BOT_NAME, BOT_USERNAME, OWNER_ID, telethn
+from TitanXManager.events import register
 
 LOGO_LINKS = [
     "https://telegra.ph/file/d1838efdafce9fe611d0c.jpg",
@@ -274,7 +274,7 @@ async def lego(event):
         draw.text(
             (x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black"
         )
-        fname = "fallen.png"
+        fname = "titan.png"
         img.save(fname, "png")
         await telethn.send_file(
             event.chat_id,
@@ -290,7 +290,7 @@ async def lego(event):
         img = Image.open(io.BytesIO(requests.get(randc).content))
         draw = ImageDraw.Draw(img)
         image_widthz, image_heightz = img.size
-        fnt = glob.glob("./FallenRobot/resources/fonts/*")
+        fnt = glob.glob("./TitanXManager/resources/fonts/*")
         randf = random.choice(fnt)
         font = ImageFont.truetype(randf, 120)
         w, h = draw.textsize(text, font=font)
