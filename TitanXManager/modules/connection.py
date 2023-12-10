ff05d0a2b1913e10a7a49269ea3cf4d156495b8d
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-10 20:19:33 +0530

    connection.py

--- TitanXManager/modules/connection.py
+++ TitanXManager/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackQueryHandler, CommandHandler
 
-import FallenRobot.modules.sql.connection_sql as sql
-from FallenRobot import DEV_USERS, DRAGONS, dispatcher
-from FallenRobot.modules.helper_funcs import chat_status
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
+import TitanXManager.modules.sql.connection_sql as sql
+from TitanXManager import DEV_USERS, DRAGONS, dispatcher
+from TitanXManager.modules.helper_funcs import chat_status
+from TitanXManager.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
