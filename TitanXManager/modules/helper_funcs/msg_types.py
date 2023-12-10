a050600e0e97f6a4526729311cf2ad63e5872b04
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-10 19:50:33 +0530

    Update msg_types.py

--- TitanXManager/modules/helper_funcs/msg_types.py
+++ TitanXManager/modules/helper_funcs/msg_types.py
@@ -2,7 +2,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from FallenRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from TitanXManager.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
