be70ab6c1587f8460366b26a0ebaa734d32f801d
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-10 21:51:19 +0530

    Update fun.py

--- TitanXManager/modules/fun.py
+++ TitanXManager/modules/fun.py
@@ -6,11 +6,11 @@ from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext
 
-import FallenRobot.modules.fun_strings as fun_strings
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import is_user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_user
+import TitanXManager.modules.fun_strings as fun_strings
+from TitanXManager import dispatcher
+from TitanXManager.modules.disable import DisableAbleCommandHandler
+from TitanXManager.modules.helper_funcs.chat_status import is_user_admin
+from TitanXManager.modules.helper_funcs.extraction import extract_user
 
 
 def runs(update: Update, context: CallbackContext):
