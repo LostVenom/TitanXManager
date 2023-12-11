fe99c7f7bb06cb4fe050469f26f412c401705f57
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-11 13:54:14 +0530

    google.py

--- TitanXManager/modules/google.py
+++ TitanXManager/modules/google.py
@@ -256,7 +256,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Fallen <==="
+        app_details += "\n\n===> Titan <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
