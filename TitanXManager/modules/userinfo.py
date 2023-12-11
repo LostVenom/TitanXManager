f3bfa1e9d6e3a251214550f2957b3466fae689cb
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-11 14:06:49 +0530

    userinfo.py

--- TitanXManager/modules/userinfo.py
+++ TitanXManager/modules/userinfo.py
@@ -301,10 +301,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/FallenAssociation/7"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/TitanXSupport"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/FallenAssociation/8"
+                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/TitanXSupport"
                             ),
                         ],
                     ]
