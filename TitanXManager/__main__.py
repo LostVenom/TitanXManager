421667252e0eebd7ca1b1b245a46d5767d358835
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-12 10:06:42 +0530

    Update __main__.py

--- TitanXManager/__main__.py
+++ TitanXManager/__main__.py
@@ -206,7 +206,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAEK8u5ld-MJ2tVwCGRvhE1T35NkSKhvggAC2AsAAksJ2VYLiwABIk-zkxIzBA"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
