d953f406f3f4fddfcf970525c2a1b45b5f444a51
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2023-12-18 23:41:34 +0530

    misc.py

--- TitanXManager/modules/helper_funcs/misc.py
+++ TitanXManager/modules/helper_funcs/misc.py
@@ -94,7 +94,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
     else:
-        pairs += [[EqInlineKeyboardButton("[🔙]",  callback_data="titan_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙",  callback_data="titan_back")]]
 
     return pairs
 
